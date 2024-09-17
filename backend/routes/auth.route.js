3cc481f92b68c3d125d54f7b13a20195338fba46
ujjwal <ujjwaltomar.0808@gmail.com>
2024-09-18 00:13:09 +0530

    resolve some errors

--- backend/routes/auth.route.js
+++ backend/routes/auth.route.js
@@ -14,4 +14,9 @@ router.post("/login", login);
 router.post("/logout", logout);
 
 
+// router.post("/verify-email", verifyEmail);
+// router.post("/forgot-password", forgotPassword);
+
+// router.post("/reset-password/:token", resetPassword);
+
 export default router;
