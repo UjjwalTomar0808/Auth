c55ba38b814a9d1a998a2033e89bef604b3129ab
ujjwal <ujjwaltomar.0808@gmail.com>
2024-09-24 23:23:06 +0530

    adding input component

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,8 +1,10 @@
 function App() {
   return (
-    <h1 className="text-3xl font-bold underline">
-    Hello world!
-    </h1>
+    <div>
+      <FloatingShape color='bg-green-500' size='w-64 h-64' top='-5%' left='10%' delay={0} />
+			<FloatingShape color='bg-emerald-500' size='w-48 h-48' top='70%' left='80%' delay={5} />
+			<FloatingShape color='bg-lime-500' size='w-32 h-32' top='40%' left='-10%' delay={2} />
+    </div>
   )
 }
 export default App
